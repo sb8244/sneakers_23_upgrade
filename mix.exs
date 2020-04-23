d4de1749f4f4771a119f3b6ffe19eca18a74224f
Stephen Bussey <sb8244@cs.ship.edu>
2020-04-23 12:47:15 -0400

    Bump libs

--- mix.exs
+++ mix.exs
@@ -41,8 +41,8 @@ defmodule Sneakers23.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.7"},
-      {:phoenix_pubsub, "~> 1.1"},
+      {:phoenix, "~> 1.5.0-rc0"},
+      {:phoenix_pubsub, "~> 2.0"},
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.0"},
       {:postgrex, ">= 0.0.0"},
@@ -50,7 +50,7 @@ defmodule Sneakers23.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
+      {:plug_cowboy, "~> 2.1"},
       {:hound, "~> 1.0"},
       {:basic_auth, "~> 2.2.2"}
     ]
