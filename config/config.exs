887acb9c971faa140bcbf2a7ad9376292b4d7df8
Stephen Bussey <sb8244@cs.ship.edu>
2020-04-23 12:47:23 -0400

    Configure the new pubsub

--- config/config.exs
+++ config/config.exs
@@ -24,7 +24,7 @@ config :sneakers_23, Sneakers23Web.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "/pQ3LrKcYNFtUJQ5XQBobICQGWUFohT3O6yuz+76ZuzA2khRwvIBpFJx6BTeFTZ/",
   render_errors: [view: Sneakers23Web.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Sneakers23.PubSub, adapter: Phoenix.PubSub.PG2]
+  pubsub_server: Sneakers23.PubSub
 
 # Configures Elixir's Logger
 config :logger, :console,
--- lib/sneakers_23/application.ex
+++ lib/sneakers_23/application.ex
@@ -14,6 +14,7 @@ defmodule Sneakers23.Application do
   def start(_type, _args) do
     children = [
       Sneakers23.Repo,
+      {Phoenix.PubSub, name: Sneakers23.PubSub},
       Sneakers23Web.Endpoint,
       {Sneakers23Web.CartTracker,
         [pool_size: :erlang.system_info(:schedulers_online)]},
